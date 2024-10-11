74ae0502a7ac24f1fa71810378ef036e40705fa9
paulz <z0rge@bk.ru>
2024-10-11 07:40:06 +0300

    - update plugin metadata

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,4 +1,4 @@
-import { t, ChartMetadata, ChartPlugin } from '@superset-ui/core';
+import { t, ChartMetadata, ChartPlugin, Behavior } from '@superset-ui/core';
 import buildQuery from './buildQuery';
 import controlPanel from './controlPanel';
 import thumbnail from '../images/paulz-thumbnail.png';
@@ -6,10 +6,28 @@ import transformProps from './transformProps';
 import { PaulzTableFormData, PaulzTableProps } from '../types';
 
 const metadata = new ChartMetadata({
+  behaviors: [
+    Behavior.InteractiveChart,
+    Behavior.DrillToDetail,
+    Behavior.DrillBy,
+  ],
+  category: t('Table'),
+  canBeAnnotationTypes: ['EVENT', 'INTERVAL'],
   description:
     'Используйте таблицы, чтобы продемонстрировать представление базовых данных, показать агрегированные показатели или для суммирования набора данных путем группировки нескольких статистических данных по двум осям.',
   name: t('PaulZ Table'),
+  tags: [
+    t('Additive'),
+    t('Business'),
+    t('Pattern'),
+    t('Popular'),
+    t('Report'),
+    t('Sequential'),
+    t('Tabular'),
+    t('Description'),
+  ],
   thumbnail,
+  exampleGallery: [{ url: '' }],
 });
 
 export default class PaulzTable extends ChartPlugin<
